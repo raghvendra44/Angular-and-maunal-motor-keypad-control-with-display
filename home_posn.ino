c000b5132ba8a180a6d9688e2ab39028a6c58d3e
raghvendra44 <68062751+raghvendra44@users.noreply.github.com>
2022-01-20 09:41:38 +0530

    Update home_posn.ino

--- home_posn.ino
+++ home_posn.ino
@@ -1,22 +1,25 @@
-void home_posn()
+byte home_posn()
 {
+   
    int slave_id = 7;
-  
+   rmcs.Speed(slave_id,400);
+   rmcs.Acceleration(slave_id,600);
    rmcs.Absolute_position(slave_id,0);
-   lcd.setCursor(0,1);lcd.print("Angle:");
-   while(1)       //Keep reading positions. Exit when reached.
+   lcd.clear();lcd.setCursor(0,1);lcd.print("Angle:");
+   while(fin == 1)       //Keep reading positions. Exit when reached.
    {
-      lcd.setCursor(6,1);lcd.print(float(rmcs.Position_Feedback(slave_id))/23.00);
-      delay(10);
+      lcd.setCursor(7,1);lcd.print(float(rmcs.Position_Feedback(slave_id))/23.00);
+      //delay(10);
       
       if(rmcs.Position_Feedback(slave_id)==0)
       {
          Serial.println(F("Home Position reached"));
-         lcd.setCursor(0,0);lcd.print("ANGL");
+         lcd.setCursor(0,0);lcd.print("HOME POSN");
          lcd.setCursor(0,1);lcd.print("Done :");
          lcd.setCursor(6,1);lcd.print(float(rmcs.Position_Feedback(slave_id))/23);
          break;
       }
    }
    delay(2000);
+   return 0;
 }
